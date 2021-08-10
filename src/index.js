a9483649cf9d5c2348f8e41e04867a798b764905
Simon Risberg <sirsimon40@gmail.com>
2021-08-10 12:33:54 +0200

    Created one MainPage and one SearchPage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import MainPage from './pages/MainPage'
+import SearchPage from './pages/SearchPage'
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
