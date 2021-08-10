e794aad5f55074f6c9bdddb60550ee7b1a39bb64
Simon Risberg <sirsimon40@gmail.com>
2021-08-10 13:35:32 +0200

    Added router functionality to be able to switch between mainpage and searchpage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import MainPage from './pages/MainPage'
-import SearchPage from './pages/SearchPage'
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
