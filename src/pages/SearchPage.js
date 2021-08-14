fc196d27e5425904b3bb927ec1759e0276eca073
Simon Risberg <sirsimon40@gmail.com>
2021-08-14 16:13:22 +0200

    Added functionality to be able to search for books and change the shelf of a particular book in the search page

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,33 +1,78 @@
 import React, { Component } from 'react'
+import * as BooksAPI from '../BooksAPI'
 import { Link } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
 import '../App.css'
 
 class SearchPage extends Component {
 
-  render () {
+  state = {
+    searchQuery: ""
+  }
+
+  updateQuery = (searchQuery) => {
+    this.setState(() => ({
+      searchQuery: searchQuery.trim()
+    }))
+  }
+
+
+  render() {
+    const { searchQuery } = this.state
     const { books } = this.props
+    const searchedBooks = searchQuery === '' ? books : books.filter((book) => (
+      book.title.toLowerCase().includes(searchQuery.toLocaleLowerCase())
+    ))
+
+    const fetchAuthors = (authors) => (
+      authors.toString()
+    )
+
     console.log(books)
     return (
       <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to={'/'}>Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
+        <div className="search-books-bar">
+          <Link className="close-search" to={'/'}>Close</Link>
+          <div className="search-books-input-wrapper">
+            {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={searchQuery}
+              onChange={(e) => this.updateQuery(e.target.value)} />
 
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
           </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {searchedBooks.map((book) => (
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-shelf-changer">
+                      <select value={book.shelf} onChange={(e) => BooksAPI.update(book, e.target.value)}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">{fetchAuthors(book.authors)}</div>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
     )
   }
 }
