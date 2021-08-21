6a319d2477af6c86a991971ba701ae16e60717c7
Simon Risberg <sirsimon40@gmail.com>
2021-08-21 12:59:51 +0200

    Moved around state but still have problem with update shelf on SearchPage

--- src/App.js
+++ src/App.js
@@ -8,35 +8,76 @@ import SearchPage from './pages/SearchPage'
 class App extends React.Component {
 
   state = {
-    books: []
+    books: [],
+    searchQuery: "",
+    searchedBooks: []
   }
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState(() => ({
-        books
+        books: books || []
       }))
     })
   }
 
-  handleOnChange =(book, shelf) => {
-    BooksAPI.update(book, shelf)
-    BooksAPI.getAll().then((books) => {
-      this.setState(() => ({
-        books
-      }))
-    })
+  syncBooks = (searchedBooks) => {
+    const ownedBooks = this.state.books
+    let newBooks = []
+    if (searchedBooks.length > 0) {
+      newBooks = searchedBooks.map((searchedBook) => {
+        const ownedBook = ownedBooks.find((owned) => owned.id === searchedBook.id)
+        return ownedBook ? ownedBook : searchedBook
+      })
+    }
+    this.setState(() => ({
+      searchedBooks: newBooks
+    }))
+
+  }
+
+  searchBooks = (query) => {
+    if (query) {
+      BooksAPI.search(query)
+        .then((books) => {
+          console.log('books', books)
+          if (query) {
+            this.syncBooks(books)
+          } else {
+            this.setState({ searchedBooks: [] })
+          }
+        })
+    }
+  }
+
+  updateQuery = (searchQuery) => {
+    this.setState(() => ({
+      searchQuery: searchQuery
+    }))
+    this.searchBooks(searchQuery)
   }
   
+  handleOnChange = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(() => {
+      BooksAPI.getAll().then((books) => {
+        this.setState({ books: books })
+      })
+    })
+  }
+
   render() {
     return (
       <Router>
-        <Route exact path='/' render={() => (
+        <Route exact path='/'>
           <MainPage ownedBooks={this.state.books} updateBookShelf={this.handleOnChange} />
-        )} />
-        <Route path='/search' render={() => (
-          <SearchPage ownedBooks={this.state.books} updateBookShelf={this.handleOnChange} />
-        )} />
+        </Route>
+        <Route path='/search'>
+          <SearchPage
+            ownedBooks={this.state.books}
+            updateBookShelf={this.handleOnChange}
+            searchedBooks={this.state.searchedBooks}
+            updateQuery={this.updateQuery} />
+        </Route>
       </Router>
     )
   }
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,74 +1,11 @@
 import React, { Component } from 'react'
-import * as BooksAPI from '../BooksAPI'
 import { Link } from 'react-router-dom'
 import MissingImage from '../icons/book-pack.svg'
 import '../App.css'
 
 class SearchPage extends Component {
-
-  state = {
-    searchQuery: "",
-    books: []
-  }
-
-  fetchBooks = (query) => {
-    if (this.state.searchQuery && this.state.searchQuery != null) {
-      BooksAPI.search(this.state.searchQuery)
-        .then((books) => {
-          console.log('books', books)
-          if (query !== "") {
-            this.syncBooks(books)
-          } else {
-            this.setState({ books: [] })
-          }
-
-        })
-    }
-  }
-
-  updateQuery = (searchQuery) => {
-    this.setState(() => ({
-      searchQuery: searchQuery
-    }))
-    this.fetchBooks(searchQuery)
-  }
-
-  syncBooks = (searchedBooks) => {
-    const { ownedBooks } = this.props
-    let newBooks = []
-    if (searchedBooks.length > 0) {
-      newBooks = searchedBooks.map((searchedBook) => {
-        const ownedBook = ownedBooks.find((owned) => owned.id === searchedBook.id)
-        return ownedBook ? ownedBook : searchedBook
-      })
-    }
-    this.setState(() => ({
-      books: newBooks
-    }))
-
-  }
-
-  changeBookShelf = (updatedBook, shelf) => {
-    this.props.updateBookShelf(updatedBook, shelf)
-    const books = this.state.books
-    let updatedBooks = []
-
-    updatedBooks = books.map((book) => {
-      const bookToBeUpdated = books.find((book) => book.id === updatedBook.id)
-      
-      return bookToBeUpdated ? bookToBeUpdated["shelf"] = shelf : book
-    })
-    this.setState(() => ({
-      books: updatedBooks
-    }))
-
-
-  }
-
   render() {
-    const { searchQuery } = this.state
-    const books = this.state.books
-    const { updateBookShelf } = this.props
+    const { updateBookShelf, searchedBooks, updateQuery, searchQuery } = this.props
 
     const fetchAuthors = (authors) => (
       authors != null ? authors.toString() : "Anonymous"
@@ -83,15 +20,15 @@ class SearchPage extends Component {
               type="text"
               placeholder="Search by title or author"
               value={searchQuery}
-              onChange={(e) => this.updateQuery(e.target.value)} />
+              onChange={(e) => updateQuery(e.target.value)} />
 
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {books.length === 0 || books.error === "empty query" ? (<div>
-              <span>No books to be shown</span>
-            </div>) : books.map((book) => (
+            {searchedBooks.length === 0 || searchedBooks.error === "empty query" ? (<div>
+              <span>No searchedBooks to be shown</span>
+            </div>) : searchedBooks.map((book) => (
               <li key={book.id}>
                 <div className="book">
                   <div className="book-top">
