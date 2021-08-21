351959cba467bcd498dfd7dead52e5b912c42b1c
Simon Risberg <sirsimon40@gmail.com>
2021-08-21 12:02:34 +0200

    Added a function to changebookShelf on book state in searchpage

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -16,12 +16,12 @@ class SearchPage extends Component {
       BooksAPI.search(this.state.searchQuery)
         .then((books) => {
           console.log('books', books)
-          if(query !== "") {
+          if (query !== "") {
             this.syncBooks(books)
-          }else{
-            this.setState({books: []})
+          } else {
+            this.setState({ books: [] })
           }
-          
+
         })
     }
   }
@@ -36,7 +36,7 @@ class SearchPage extends Component {
   syncBooks = (searchedBooks) => {
     const { ownedBooks } = this.props
     let newBooks = []
-    if(searchedBooks.length > 0) {
+    if (searchedBooks.length > 0) {
       newBooks = searchedBooks.map((searchedBook) => {
         const ownedBook = ownedBooks.find((owned) => owned.id === searchedBook.id)
         return ownedBook ? ownedBook : searchedBook
@@ -45,6 +45,23 @@ class SearchPage extends Component {
     this.setState(() => ({
       books: newBooks
     }))
+
+  }
+
+  changeBookShelf = (updatedBook, shelf) => {
+    this.props.updateBookShelf(updatedBook, shelf)
+    const books = this.state.books
+    let updatedBooks = []
+
+    updatedBooks = books.map((book) => {
+      const bookToBeUpdated = books.find((book) => book.id === updatedBook.id)
+      
+      return bookToBeUpdated ? bookToBeUpdated["shelf"] = shelf : book
+    })
+    this.setState(() => ({
+      books: updatedBooks
+    }))
+
 
   }
 
@@ -62,13 +79,6 @@ class SearchPage extends Component {
         <div className="search-books-bar">
           <Link className="close-search" to={'/'}>Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
             <input
               type="text"
               placeholder="Search by title or author"
@@ -85,7 +95,7 @@ class SearchPage extends Component {
               <li key={book.id}>
                 <div className="book">
                   <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : `${MissingImage}` })` }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : `${MissingImage}`})` }}></div>
                     <div className="book-shelf-changer">
                       <select value={book.shelf || 'none'} onChange={(e) => updateBookShelf(book, e.target.value)}>
                         <option value="move" disabled>Move to...</option>
